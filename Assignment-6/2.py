2d143241784ab8fae1d95e3b599e6c0491cfa0dd
pranitha-21 <pranithapranitha388@gmail.com>
2025-03-03 23:29:11 +0530

    Add files via upload

---
+++ Assignment-6/2.py
@@ -0,0 +1,20 @@
+T=int(input("Enter no. of test cases:"))
+print("Enter Numbers:")
+l=[]
+for i in range(T):
+    a=int(input())
+    l.append(a)
+i=0
+k=1
+f=[0]
+while max(l)>i:
+    b=i
+    i=k
+    k=k+b
+    f.append(i)
+print(f)
+for i in range(T):
+    if l[i] in f:
+        print("{l[i]} IsFibo")
+    else:
+        print(f"{l[i]} IsnotFibo")
