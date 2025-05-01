e2ec8797ef7949adeb6c1e1a6043d63b468f6031
pranitha-21 <pranithapranitha388@gmail.com>
2025-05-01 14:11:51 +0530

    1.py

--- Assignment-1/1.py
+++ Assignment-1/1.py
@@ -1,17 +1,18 @@
-#include <stdio.h>
-void main(){
-	float principle,time,rate,simpleinterest;
-	
-	printf("Enter Principle:");
-	scanf("%f",&principle);
-	
-	printf("Enter Time:");
-	scanf("%f",&time);
-	
-	printf("Enter Rate:");
-	scanf("%f",&rate);
-	
-	simpleinterest=(principle*time*rate)/100;
-	printf("simple interest is %f",simpleinterest);
-	
-	}
+
+#list of numbers from 0 to 49
+num=[]
+for i in range(50):
+    num.append(i)
+print(num)
+
+#squares of integers from 1 to 50
+square=[]
+for i in range(51):
+    square.append(i**2)
+print(square)
+
+#list containing ['a','bb',.....]
+a=[]
+for i in range(26):
+    a.append(chr((ord('a')+i))*(i+1))
+print(a)
